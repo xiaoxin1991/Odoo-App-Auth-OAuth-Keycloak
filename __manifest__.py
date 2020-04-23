a487e18c7afc3641b6d5439cfb7d3c96a03038d0
Janik von Rotz <login@janikvonrotz.ch>
2020-04-23 14:45:17 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "auth_oauth_keycloack",
+
+    'summary': """
+        Enables Keycloack integration for the OAuth2 Authentication module.""",
+
+    'description': """
+        Enables Keycloack integration for the OAuth2 Authentication module.
+    """,
+
+    'author': "Mint System",
+    'website': "https://www.mint-system.ch",
+
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'auth_oauth',
+    ],
+
+    'data': [
+        'views/ir_model_fields.xml',
+        'views/ir_ui_view.xml',
+    ],
+}
